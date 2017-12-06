59507e3bd1b75aaec542f93f24c9bc91a6cb4593
Denis <poythn@gmail.com>
2017-12-06 17:12:23 +0700

    refactor

--- android/src/main/java/com/callerid/CallerIDReactPackage.java
+++ android/src/main/java/com/callerid/CallerIDReactPackage.java
@@ -5,6 +5,9 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import com.callerid.db.UsersDB;
+import com.callerid.notif.AddPermissions;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
