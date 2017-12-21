fbb8b0cf793ef066c73cc459e65278a44fae1924
Denis <poythn@gmail.com>
2017-12-21 16:00:59 +0700

    Fix errors with empty fields

--- android/src/main/java/com/callerid/db/UsersDB.java
+++ android/src/main/java/com/callerid/db/UsersDB.java
@@ -4,6 +4,10 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableArray;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,29 +34,40 @@ public class UsersDB extends ReactContextBaseJavaModule {
         try {
 //            long startTime = System.nanoTime();
             List<User> newUsers = new ArrayList<>();
+            WritableArray declainedUsers = new WritableNativeArray();
             JSONArray arr = new JSONArray(options);
             DataBase database = DataBase.getDatabase(getReactApplicationContext(), passPhrase);
-            if(null == database) {
-                callback.invoke(new JSONException("Caller-ID database cannot be created"));
-                return;
-            }
+
+            if(null == database) throw new JSONException("Caller-ID database cannot be created");
+
             database.userDao().removeAllUsers();
             for (int i=0; i < arr.length(); i++) {
-                JSONObject o = arr.getJSONObject(i);
-                String name = o.getString("name");
-                String number = o.getString("number");
-                String i164 = o.getString("i164");
-                newUsers.add(new User(name, number, i164));
+                String name = null;
+                String number = null;
+                String i164 = null;
+                try {
+                    JSONObject o = arr.getJSONObject(i);
+                    name = o.getString("name");
+                    number = o.getString("number");
+                    i164 = o.getString("i164");
+                    newUsers.add(new User(name, number, i164));
+                } catch (JSONException e) {
+                    WritableMap declainedUser = new WritableNativeMap();
+                    declainedUser.putString("name", name);
+                    declainedUser.putString("number", number);
+                    declainedUser.putString("i164", i164);
+                    declainedUsers.pushMap(declainedUser);
+                }
             }
             database.userDao().addUsers(newUsers);
 //            long endTime = System.nanoTime();
 //            long duration = (endTime - startTime);
 //            Toast.makeText(getReactApplicationContext(), "Users added to DB. Duration: " + (duration / 1000000) + " msec", Toast.LENGTH_LONG).show();
             Toast.makeText(getReactApplicationContext(), "Users added to Caller-ID", Toast.LENGTH_LONG).show();
-            callback.invoke();
+            callback.invoke(null, declainedUsers);
         } catch (JSONException e) {
             e.printStackTrace();
-            callback.invoke(e);
+            callback.invoke(e.getMessage());
         }
     }
 }
--- index.js
+++ index.js
@@ -15,13 +15,14 @@ const callerId = {
           Alert.alert("Notification access",
               "For proper work of Caller ID the application needs access to be given in settings",
               [{text: "Ok",
-                onPress: () => NativeModules.AddPermissions.notificationsAccess()}]);
+                onPress: () => NativeModules.AddPermissions.notificationsAccess()}],
+              { cancelable: false });
         })
       }).catch(e => rej(e))),
 
   getNotificationsAccess: () => new Promise(res => NativeModules.AddPermissions.getNotificationsAccess().then((access) => res(access))),
 
-  uploadUsers: (users, passphrase)=> new Promise((res, rej) => NativeModules.UsersDB.upload(JSON.stringify(users), passphrase || "", (err) => err?rej(err):res())),
+  uploadUsers: (users, passphrase)=> new Promise((res, rej) => NativeModules.UsersDB.upload(JSON.stringify(users), passphrase || "", (err, declained) => err?rej(err):res(declained))),
 };
 
 export default callerId;
